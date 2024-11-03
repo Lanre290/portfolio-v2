2d66e8e8be605f9c2555c95253b6d702b716adad
Ashiru Olanrewaju <lanre2967@gmail.com>
2024-11-03 10:50:06 +0100

    updates #5

--- src/components/certifications.tsx
+++ src/components/certifications.tsx
@@ -1,43 +1,85 @@
 import Title from "./Title";
-import studentExpert from './../assets/student expert.png';
-import ALX from './../assets/alx.jpeg';
-import web_dev from './../assets/web.dev.svg';
+import studentExpert from "./../assets/student expert.png";
+import ALX from "./../assets/alx.jpeg";
+import web_dev from "./../assets/web.dev.svg";
+import { BsArrowRight } from "react-icons/bs";
 
-function Certifications(){
-    return(
-        <div className="flex flex-col w-full md:w-5/6 lg:4/6 xl:3/6 mb-24 gap-y-7">
-            <Title text="Certifications"></Title>
-            <div className="flex flex-row flex-wrap justify-center items-center gap-2">
-                <a href="https://badgr.com/public/assertions/JmZwwo3gQnGxA1eXBBd-0g?identity__email=lanre2967@gmail.com" target="_blank" className="p-6 bg-gray-500 bg-opacity-5 flex flex-col justify-start items-center w-2/3 md:w-1/3 lg:w-1/4 h-auto md:h-64">
-                    <div className="w-16 h-16 md:w-28 md:h-28 rounded-full flex items-center justify-center bg-black bg-center bg-cover bg-no-repeat">
-                        <img src={studentExpert} alt="postman student expert" className="w-full h-full object-cover rounded-full" />
-                    </div>
-                    <h3 className="text-center text-gray-50 md:text-2xl ml-3">
-                        Postman API Fundamentals Student Expert
-                    </h3>
-                    <h3 className="text-center primary-color mt-3">Completed</h3>
-                </a>
-                <a href="https://intranet.alxswe.com/certificates/sEGyJPLeBh" target="_blank" className="p-6 bg-gray-500 bg-opacity-5 flex flex-col justify-start items-center w-2/3 md:w-1/3 lg:w-1/4 h-auto md:h-64">
-                    <div className="w-16 h-16 md:w-28 md:h-28 rounded-full flex items-center justify-center bg-black">
-                        <img src={ALX} alt="postman student expert" className="w-full h-full object-cover rounded-full" />
-                    </div>
-                    <h3 className="text-center text-gray-50 md:text-2xl ml-3">
-                        ALX Professional Foundational Course
-                    </h3>
-                    <h3 className="text-center primary-color mt-3">Completed</h3>
-                </a>
-                <a className="p-6 bg-gray-500 bg-opacity-5 flex flex-col justify-start items-center w-2/3 md:w-1/3 lg:w-1/4 h-auto md:h-64">
-                    <div className="w-16 h-16 md:w-28 md:h-28 rounded-full flex items-center justify-center bg-black">
-                        <img src={web_dev} alt="Google certification" className="w-full h-full object-cover rounded-full" />
-                    </div>
-                    <h3 className="text-center text-gray-50 md:text-2xl ml-3">
-                        Google (Web.dev) - User and Data Privacy
-                    </h3>
-                    <h3 className="text-center primary-color mt-3">Completed</h3>
-                </a>
-            </div>
-        </div>
-    )
+function Certifications() {
+  return (
+    <div className="flex flex-col w-full md:w-5/6 lg:4/6 xl:3/6 mb-24 gap-y-7">
+      <Title text="Certifications"></Title>
+      <div className="flex flex-row flex-wrap justify-center items-center gap-2">
+        <a
+          href="https://badgr.com/public/assertions/JmZwwo3gQnGxA1eXBBd-0g?identity__email=lanre2967@gmail.com"
+          target="_blank"
+          className="p-6 bg-gray-500 bg-opacity-5 flex flex-col justify-start items-center w-2/3 md:w-1/3 lg:w-1/4 h-auto md:h-64"
+        >
+          <div className="w-16 h-16 md:w-28 md:h-28 rounded-full flex items-center justify-center bg-black bg-center bg-cover bg-no-repeat">
+            <img
+              src={studentExpert}
+              alt="postman student expert"
+              className="w-full h-full object-cover rounded-full"
+            />
+          </div>
+          <h3 className="text-center text-gray-50 md:text-2xl ml-3">
+            Postman API Fundamentals Student Expert
+          </h3>
+          <h3 className="text-center primary-color mt-3">Completed</h3>
+          <a
+            href={'https://badgr.com/public/assertions/JmZwwo3gQnGxA1eXBBd-0g?identity__email=lanre2967@gmail.com'}
+            target="_blank"
+            className="w-full flex justify-center items-center py-3 cursor-pointer primary-color hover:bg-gray-50 hover:bg-opacity-10 gap-x-3"
+          >
+            View <BsArrowRight className="text-2xl"></BsArrowRight>
+          </a>
+        </a>
+        <a
+          href="https://intranet.alxswe.com/certificates/sEGyJPLeBh"
+          target="_blank"
+          className="p-6 bg-gray-500 bg-opacity-5 flex flex-col justify-start items-center w-2/3 md:w-1/3 lg:w-1/4 h-auto md:h-64"
+        >
+          <div className="w-16 h-16 md:w-28 md:h-28 rounded-full flex items-center justify-center bg-black">
+            <img
+              src={ALX}
+              alt="postman student expert"
+              className="w-full h-full object-cover rounded-full"
+            />
+          </div>
+          <h3 className="text-center text-gray-50 md:text-2xl ml-3">
+            ALX Professional Foundational Course
+          </h3>
+          <h3 className="text-center primary-color mt-3">Completed</h3>
+          <a
+            href={'https://intranet.alxswe.com/certificates/sEGyJPLeBh'}
+            target="_blank"
+            className="w-full flex justify-center items-center py-3 cursor-pointer primary-color hover:bg-gray-50 hover:bg-opacity-10 gap-x-3"
+          >
+            View <BsArrowRight className="text-2xl"></BsArrowRight>
+          </a>
+        </a>
+        <a className="p-6 bg-gray-500 bg-opacity-5 flex flex-col justify-start items-center w-2/3 md:w-1/3 lg:w-1/4 h-auto md:h-64">
+          <div className="w-16 h-16 md:w-28 md:h-28 rounded-full flex items-center justify-center bg-black">
+            <img
+              src={web_dev}
+              alt="Google certification"
+              className="w-full h-full object-cover rounded-full"
+            />
+          </div>
+          <h3 className="text-center text-gray-50 md:text-2xl ml-3">
+            Google (Web.dev) - User and Data Privacy
+          </h3>
+          <h3 className="text-center primary-color mt-3">Completed</h3>
+          <a
+            href={'#'}
+            target="_blank"
+            className="w-full flex justify-center items-center py-3 cursor-pointer primary-color hover:bg-gray-50 hover:bg-opacity-10 gap-x-3"
+          >
+            View <BsArrowRight className="text-2xl"></BsArrowRight>
+          </a>
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Certifications;
