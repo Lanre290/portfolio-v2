73e3086726d64d09d6caaa6b0211eabf7d91e5fa
Ashiru Olanrewaju <lanre2967@gmail.com>
2024-11-03 11:10:06 +0100

    update #6

--- src/components/certifications.tsx
+++ src/components/certifications.tsx
@@ -12,19 +12,19 @@ function Certifications() {
         <a
           href="https://badgr.com/public/assertions/JmZwwo3gQnGxA1eXBBd-0g?identity__email=lanre2967@gmail.com"
           target="_blank"
-          className="p-6 bg-gray-500 bg-opacity-5 flex flex-col justify-start items-center w-2/3 md:w-1/3 lg:w-1/4 h-auto md:h-64"
+          className="bg-gray-500 bg-opacity-5 flex flex-col justify-between items-center w-2/3 md:w-1/3 lg:w-1/4 h-auto md:h-80"
         >
-          <div className="w-16 h-16 md:w-28 md:h-28 rounded-full flex items-center justify-center bg-black bg-center bg-cover bg-no-repeat">
+          <div className="w-16 h-16 min-w-16 min-h-16 md:w-28 md:h-28 md:min-w-28 md:min-h-28 rounded-full flex items-center justify-center bg-black bg-center bg-cover bg-no-repeat mt-3">
             <img
               src={studentExpert}
               alt="postman student expert"
-              className="w-full h-full object-cover rounded-full"
+              className="w-full h-full min-w-full min-h-full object-cover rounded-full"
             />
           </div>
-          <h3 className="text-center text-gray-50 md:text-2xl ml-3">
+          <h3 className="text-center text-gray-50 md:text-2xl ml-3 p-2 py-0">
             Postman API Fundamentals Student Expert
           </h3>
-          <h3 className="text-center primary-color my-3">Completed</h3>
+          <h3 className="text-center primary-color">Completed</h3>
           <a
             href={'https://badgr.com/public/assertions/JmZwwo3gQnGxA1eXBBd-0g?identity__email=lanre2967@gmail.com'}
             target="_blank"
@@ -36,19 +36,19 @@ function Certifications() {
         <a
           href="https://intranet.alxswe.com/certificates/sEGyJPLeBh"
           target="_blank"
-          className="p-6 bg-gray-500 bg-opacity-5 flex flex-col justify-start items-center w-2/3 md:w-1/3 lg:w-1/4 h-auto md:h-64"
+          className="bg-gray-500 bg-opacity-5 flex flex-col justify-between items-center w-2/3 md:w-1/3 lg:w-1/4 h-auto md:h-80"
         >
-          <div className="w-16 h-16 md:w-28 md:h-28 rounded-full flex items-center justify-center bg-black">
+          <div className="w-16 h-16 min-w-16 min-h-16 md:w-28 md:h-28 md:min-w-28 md:min-h-28 rounded-full flex items-center justify-center bg-black bg-center bg-cover bg-no-repeat mt-3">
             <img
               src={ALX}
               alt="postman student expert"
-              className="w-full h-full object-cover rounded-full"
+              className="w-full h-full min-w-full min-h-full object-cover rounded-full"
             />
           </div>
-          <h3 className="text-center text-gray-50 md:text-2xl ml-3">
+          <h3 className="text-center text-gray-50 md:text-2xl ml-3 p-2 py-0">
             ALX Professional Foundational Course
           </h3>
-          <h3 className="text-center primary-color my-3">Completed</h3>
+          <h3 className="text-center primary-color">Completed</h3>
           <a
             href={'https://intranet.alxswe.com/certificates/sEGyJPLeBh'}
             target="_blank"
@@ -57,18 +57,18 @@ function Certifications() {
             View <BsArrowRight className="text-2xl"></BsArrowRight>
           </a>
         </a>
-        <a className="p-6 bg-gray-500 bg-opacity-5 flex flex-col justify-start items-center w-2/3 md:w-1/3 lg:w-1/4 h-auto md:h-64">
-          <div className="w-16 h-16 md:w-28 md:h-28 rounded-full flex items-center justify-center bg-black">
+        <a className="bg-gray-500 bg-opacity-5 flex flex-col justify-between items-center w-2/3 md:w-1/3 lg:w-1/4 h-auto md:h-80">
+          <div className="w-16 h-16 min-w-16 min-h-16 md:w-28 md:h-28 md:min-w-28 md:min-h-28 rounded-full flex items-center justify-center bg-black bg-center bg-cover bg-no-repeat mt-3">
             <img
               src={web_dev}
               alt="Google certification"
-              className="w-full h-full object-cover rounded-full"
+              className="w-full h-full min-w-full min-h-full object-cover rounded-full"
             />
           </div>
-          <h3 className="text-center text-gray-50 md:text-2xl ml-3">
+          <h3 className="text-center text-gray-50 md:text-2xl ml-3 p-2 py-0">
             Google (Web.dev) - User and Data Privacy
           </h3>
-          <h3 className="text-center primary-color my-3">Completed</h3>
+          <h3 className="text-center primary-color">Completed</h3>
           <a
             href={'#'}
             target="_blank"
